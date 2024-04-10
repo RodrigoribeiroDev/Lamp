d37dd44a4fdc4108b1483696aa79cfce743d8981
Rodrigo Ribeiro <rodrigo_ribeiro_gomes@hotmail.com>
2024-04-10 01:41:06 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var lamp = document.querySelector('.lamp')
+
+function ligar(){
+        lamp.src = 'img/ligada.jpg'
+}
+
+function desligar(){
+        lamp.src = 'img/desligada.jpg'
+}
+
+function quebrar(){
+    lamp.src = 'img/quebrada.jpg'
+}
